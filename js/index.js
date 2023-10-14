3b4973903dd7466f310f9284fc2480eba6a38f1c
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-14 23:08:50 +0300

    feat: added project releases

--- js/index.js
+++ js/index.js
@@ -22,3 +22,24 @@ const projectNewsSlider = new Swiper('.project-news__slider .swiper', {
     prevEl: '.project-news__button_prev',
   },
 });
+
+const projectReleasesSlider = new Swiper('.project-releases__slider .swiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 36,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 43,
+    },
+  },
+});
