564abf8f62c8ff7b2da0e3197c79ccf8fd75a891
Aliaksei Karzhou <alexius90e@gmail.com>
2024-09-19 00:13:39 +0300

    refactor: refactored map feature

--- js/map.js
+++ js/map.js
@@ -5,37 +5,63 @@ const mapGalleryButtons = document.querySelectorAll('.interactive-map__map-butto
 const mapItems = [...mapButtons, ...mapPins, ...mapAreas];
 const mapGalleryWrapper = document.querySelector('.interactive-map__map-wrapper');
 
-mapItems.forEach((item) => item.addEventListener('click', handleMapItemClick));
+[...mapPins, ...mapAreas].forEach((item) =>
+  item.addEventListener('click', (event) => {
+    const [_, location] = event.currentTarget.classList;
+    updateMap(location)
+  })
+);
 
-function handleMapItemClick(event) {
-  const [_, areaName] = event.currentTarget.classList;
-  const isGalleryButton = event.target.classList.contains('interactive-map__map-button-gallery');
+mapButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const isGalleryButton = event.target.classList.contains('interactive-map__map-button-gallery');
+    const location = event.currentTarget.dataset.location;
 
-  mapItems.forEach((element) => {
-    const isTheSameArea = element.classList.contains(areaName);
-    if (isTheSameArea && !isGalleryButton) element.classList.toggle('active');
-    if (isTheSameArea && isGalleryButton) element.classList.add('active');
-    if (!isTheSameArea) {
+    if (isGalleryButton) {
+      const isActive = event.target.classList.contains('active');
+      if (!isActive) {
+        [...mapButtons, ...mapPins, ...mapAreas, ...mapGalleryButtons].forEach((button) =>
+          button.classList.remove('active')
+        );
+        event.currentTarget.classList.add('active');
+        event.target.classList.add('active');
+        if (mapGalleryWrapper)
+          mapGalleryWrapper.setAttribute('class', `interactive-map__map-wrapper ${location}`);
+      } else {
+        event.currentTarget.classList.remove('active');
+        event.target.classList.remove('active');
+        const [_, location] = event.currentTarget.classList;
+        updateMap(location)
+        if (mapGalleryWrapper)
+          mapGalleryWrapper.setAttribute('class', 'interactive-map__map-wrapper');
+      }
+    } else {
+      const isActive = event.currentTarget.classList.contains('active');
+      if (isActive) {
+        mapGalleryButtons.forEach((button) => button.classList.remove('active'));
+        if (mapGalleryWrapper)
+          mapGalleryWrapper.setAttribute('class', 'interactive-map__map-wrapper');
+      } else {
+        mapButtons.forEach((button) => button.classList.remove('active'));
+        mapGalleryButtons.forEach((button) => button.classList.remove('active'));
+        if (mapGalleryWrapper)
+          mapGalleryWrapper.setAttribute('class', 'interactive-map__map-wrapper');
+        updateMap(location)
+      }
+    }
+  });
+});
+
+function updateMap(location) {
+  [...mapButtons, ...mapPins, ...mapAreas].forEach((element) => {
+    const isTheSameArea = element.classList.contains(location);
+    if (isTheSameArea) {
+      element.classList.toggle('active');
+    } else {
       element.classList.remove('active');
     }
   });
 }
-
-mapGalleryButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const isActive = event.target.classList.contains('active');
-    const location = event.currentTarget.dataset.location;
-    if (!isActive) {
-      event.currentTarget.classList.add('active');
-      if (mapGalleryWrapper)
-        mapGalleryWrapper.setAttribute('class', `interactive-map__map-wrapper ${location}`);
-    } else {
-      event.currentTarget.classList.remove('active');
-      if (mapGalleryWrapper)
-        mapGalleryWrapper.setAttribute('class', 'interactive-map__map-wrapper');
-    }
-  });
-});
 
 if (mapGalleryWrapper) {
   const mapGallerySwipers = mapGalleryWrapper.querySelectorAll('.swiper');
@@ -61,3 +87,38 @@ if (mapGalleryWrapper) {
     });
   });
 }
+
+const swiper = new Swiper('.interactive-map__plans .swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 1500,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    400: {
+      slidesPerView: 1.5,
+      spaceBetween: 16,
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 16,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
